2887c8410461015f0e0c660827c0c2ca97f60df6
Ferv0r2 <amlk31255@gmail.com>
2025-03-31 22:01:15 +0900

    Bugfix: default injected & message

--- App.tsx
+++ App.tsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import { WebView } from 'react-native-webview'
 
-const ENDPOINT = 'https//kepler-pop.wontae.net'
+const ENDPOINT = 'https://kepler-pop.wontae.net'
 
 const App = () => {
-  return <WebView source={{ uri: ENDPOINT }} style={{ flex: 1 }} />
+  const INJECTED_JAVASCRIPT = `
+    window.ReactNativeWebView.postMessage('');
+    true;
+  `
+
+  const handleMessage = (event: any) => {
+    console.log(event)
+  }
+
+  return (
+    <WebView
+      source={{ uri: ENDPOINT }}
+      style={{ flex: 1 }}
+      injectedJavaScript={INJECTED_JAVASCRIPT}
+      onMessage={handleMessage}
+    />
+  )
 }
 
 export default App
