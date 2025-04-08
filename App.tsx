6ac9beda15af10fb395367b8c625a22efe7062a2
Ferv0r2 <amlk31255@gmail.com>
2025-04-09 00:34:17 +0900

    Revert "Feat: Add back handler"
    This reverts commit 13c2126cb1584e746a7b361869759a54fb745803.

--- App.tsx
+++ App.tsx
@@ -1,36 +1,24 @@
-import React, { useEffect, useRef, useState } from 'react'
-import { BackHandler, Platform } from 'react-native'
+import React from 'react'
 import { WebView } from 'react-native-webview'
 
 const ENDPOINT = 'https://kepler-pop.wontae.net'
 
 const App = () => {
-  const webviewRef = useRef(null)
-  const [canGoBack, setCanGoBack] = useState(false)
+  const INJECTED_JAVASCRIPT = `
+    window.ReactNativeWebView.postMessage('');
+    true;
+  `
 
-  useEffect(() => {
-    // 뒤로가기 버튼 핸들러
-    const handleBackButton = () => {
-      if (canGoBack && webviewRef.current) {
-        ;(webviewRef.current as WebView).goBack()
-        return true // 뒤로가기 이벤트
-      }
-      return false // 기본 동작 (앱 종료 등)
-    }
-
-    if (Platform.OS === 'android') {
-      BackHandler.addEventListener('hardwareBackPress', handleBackButton)
-      return () => {
-        BackHandler.removeEventListener('hardwareBackPress', handleBackButton)
-      }
-    }
-  }, [canGoBack])
+  const handleMessage = (event: any) => {
+    console.log(event)
+  }
 
   return (
     <WebView
-      ref={webviewRef}
       source={{ uri: ENDPOINT }}
-      onNavigationStateChange={(navState) => setCanGoBack(navState.canGoBack)}
+      style={{ flex: 1 }}
+      injectedJavaScript={INJECTED_JAVASCRIPT}
+      onMessage={handleMessage}
     />
   )
 }
